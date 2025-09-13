069b9b8c3ef733143a38cb85ef1e86043f802db7
taolak <taolak4orcl@gmail.com>
2025-09-13 14:50:49 -0500

    added avatar

--- src/components/slider/ImageSlider.tsx
+++ src/components/slider/ImageSlider.tsx
@@ -6,9 +6,9 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import "./ImageSlider.scss";
-import artistauth1 from "../../assets/images/artistauth1.jpg";
-import artistauth2 from "../../assets/images/artistauth4.png";
-import artistauth3 from "../../assets/images/artistauth3.jpg";
+import artistauth1 from "@/assets/images/artistauth1.png";
+import artistauth2 from "@/assets/images/artistauth4.png";
+import artistauth3 from "@/assets/images/artistauth3.png";
 
 const ImageSlider: React.FC = () => {
   const slides = [
